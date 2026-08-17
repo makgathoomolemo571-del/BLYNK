075fc5c94ca49b9e6128cfd27fdcba4ea547679b
OMO <info@thegroovevig.co.za>
2026-08-17 18:56:59 +0200

    fix production

--- modules/auth/email.service.js
+++ modules/auth/email.service.js
@@ -26,6 +26,13 @@ class EmailService {
             brevo.TransactionalEmailsApiApiKeys.apiKey,
             process.env.BREVO_API_KEY
         );
+        console.log("========== SMTP CONFIG ==========");
+console.log("EMAIL_HOST:", process.env.EMAIL_HOST);
+console.log("EMAIL_PORT:", process.env.EMAIL_PORT);
+console.log("EMAIL_USER:", process.env.EMAIL_USER);
+console.log("EMAIL_FROM:", process.env.EMAIL_FROM);
+console.log("EMAIL_PASS SET:", !!process.env.EMAIL_PASS);
+console.log("=================================");
         this.transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT || 587),
